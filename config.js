4884086318204087cde4883ca90c5ea585c9af31
Promise Imadonmwinyi <91833118+Prom1234pro@users.noreply.github.com>
2024-02-13 12:01:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['2348053534789', 'Mr Bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
